aee64d1178932f2a4204b9218c625927bb2f8a98
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-07-05 15:22:05 +0200

    Starting out with Expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,21 @@ public class Hello {
 
         int myFirstNumber = (10 + 5) + (2 * 10);
 
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myFourthNumber = myFirstNumber * 2;
+        int myTotal2 = myFirstNumber + mySecondNumber + myFourthNumber;
+
+        int myLastOne = 1000 - myTotal2;
+
         System.out.println("My first number: " + myFirstNumber);
+
+        System.out.println("My total: " + myTotal);
+
+        System.out.println("My total 2: " + myTotal2);
+
+        System.out.println("My last one: " + myLastOne);
     }
 }
