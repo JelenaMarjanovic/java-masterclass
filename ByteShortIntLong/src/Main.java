585493e46478519700f871340a9bfba4e34da33a
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-07-05 19:42:30 +0200

    Primitive types - short, Short

--- ByteShortIntLong/src/Main.java
+++ ByteShortIntLong/src/Main.java
@@ -16,5 +16,11 @@ public class Main {
 
         System.out.println("Byte minimum value: " + myMinByteValue);
         System.out.println("Byte maximum value: " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short minimum value: " + myMinShortValue);
+        System.out.println("Short maximum value: " + myMaxShortValue);
     }
 }
