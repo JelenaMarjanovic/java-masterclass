4a5a40b444e3b5d2e7aac660cb989fb34d42d6ae
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-07-05 15:10:16 +0200

    Variables - basic

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFirstNumber = 5;
+
+        System.out.println("My first number: " + myFirstNumber);
     }
 }
