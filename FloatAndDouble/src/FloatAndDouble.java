541beac008b8b2d6957cbf8a15b77784cc29e8f7
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-07-06 14:45:27 +0200

    Primitive types - double, Double

--- FloatAndDouble/src/FloatAndDouble.java
+++ FloatAndDouble/src/FloatAndDouble.java
@@ -5,5 +5,11 @@ public class FloatAndDouble {
 
         System.out.println("Float minimum value: " + myMinFloatValue);
         System.out.println("Float maximum value: " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double minimum value: " + myMinDoubleValue);
+        System.out.println("Double maximum value: " + myMaxDoubleValue);
     }
 }
