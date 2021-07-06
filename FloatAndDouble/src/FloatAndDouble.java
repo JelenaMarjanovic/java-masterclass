60877258dbfc9fac2afd4fcbba527cdc9bf710a5
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-07-06 14:59:43 +0200

    float and double - casting types

--- FloatAndDouble/src/FloatAndDouble.java
+++ FloatAndDouble/src/FloatAndDouble.java
@@ -11,5 +11,11 @@ public class FloatAndDouble {
 
         System.out.println("Double minimum value: " + myMinDoubleValue);
         System.out.println("Double maximum value: " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        // float myFloatValue = (float) 5.25;
+        double myDoubleValue = 5.25d;
+        // double myDoubleValue = 5.25;
     }
 }
